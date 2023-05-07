a9851777c4e23843112b550ade1f16edf7467cc2
prakash jaiswal <prakashjaiswal625@gmail.com>
2023-05-07 18:31:38 +0530

    Add files via upload

---
+++ routes/users-routes.js
@@ -0,0 +1,16 @@
+// routes.js
+const express = require('express');
+const router = express.Router();
+
+
+// // Define routes and their corresponding controller functions
+// router.post('/bucket/:bucketName', createBuckets); // Route to create  bucket 
+
+// router.get('/buckets', listBuckets); // Route to list all buckets
+
+
+// router.delete('/objects/:bucketName/:ObjectName', deleteObject); // Route to delete an object from a bucket
+
+// router.post('/objects/:bucketName', upload.array('files'), putObjects); //  Route to upload multiple new object to a bucket
+
+module.exports = router; // Export the router object for use in other modules
