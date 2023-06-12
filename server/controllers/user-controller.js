ea242ea68d2512af7982abf4b5d1644a0be3512e
Insanitarius <prototype.shubhodeep@gmail.com>
2023-06-12 14:54:14 +0530

    Added comments to the controller and service

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,16 +1,20 @@
-// ['firstname','lastname','email','phonenumber','profession'];
+// Import required modules and services
 const userService = require("../service/user-service");
 const validator = require("validator");
 
+// Define an array of required fields
 const requiredFields = ["name", "email", "phone", "address"];
 
+// Function to check if all required fields are present in user info
 const hasAllFields = (userInfo) => {
 	return requiredFields.every((property) => {
 		return userInfo.hasOwnProperty(property);
 	});
 };
+// One line Function to check if all required fields are present in user info
 // const hasAllFields = (userInfo) => requiredFields.every((property) => userInfo.hasOwnProperty(property));
 
+// Function to check if all required fields are present in user info
 // const hasAllFields = (userInfo) => {
 // 	if (!userInfo.name || !userInfo.email || !userInfo.phone || !userInfo.address) {
 // 		return false;
@@ -18,18 +22,27 @@ const hasAllFields = (userInfo) => {
 // 		return true;
 // 	}
 // };
+
+// One line Function to check if all required fields are present in user info
 // const hasAllFields = (userInfo) => userInfo.name && userInfo.email && userInfo.phone && userInfo.address;
 
+// Controller function to create a new user
 const createUser = async (req, res) => {
 	try {
 		let userInfo = req.body;
+
+		// Check if the user info has all the required fields
 		if (hasAllFields(userInfo)) {
+			// Call the registerUser function from the userService to create the user
 			let response = await userService.registerUser(userInfo);
+
+			// Send success response
 			res.status(200).send({
 				message: "User created successfully!",
 				result: response
 			});
 		} else {
+			// Throw an error if any required field is missing
 			throw new Error("All fields are required.");
 		}
 	} catch (error) {
@@ -38,14 +51,21 @@ const createUser = async (req, res) => {
 	}
 };
 
+// Controller function to get user information
 const getUserInfo = async (req, res) => {
 	try {
 		let { userId } = req.params;
+
+		// Check if the provided userId is a valid MongoDB id
 		if (!validator.isMongoId(userId)) {
 			throw new Error("Invalid user id!");
 		}
+
+		// Call the getInfo function from the userService to fetch the user information
 		let response = await userService.getInfo(userId);
+
 		if (response) {
+			// Send success response with the fetched user information
 			res.status(200).send({
 				message: "User Data Fetched Successfully",
 				results: response
@@ -57,9 +77,13 @@ const getUserInfo = async (req, res) => {
 	}
 };
 
+// Controller function to list all users
 const listAllUsers = async (req, res) => {
 	try {
+		// Call the listAllUsers function from the userService to get all users
 		let allUsers = await userService.listAllUsers();
+
+		// Send success response with the list of all users
 		res.status(200).send(allUsers);
 	} catch (error) {
 		console.log("Error occurred while listing all users: ", error);
@@ -67,15 +91,21 @@ const listAllUsers = async (req, res) => {
 	}
 };
 
+// Controller function to update user information
 const updateUser = async (req, res) => {
 	try {
 		let { userId } = req.params;
 		let userData = req.body;
+
+		// Check if the provided userId is a valid MongoDB id
 		if (!validator.isMongoId(userId)) {
 			throw new Error("Invalid user id!");
 		}
+
+		// Call the updateUser function from the userService to update the user information
 		let response = await userService.updateUser(userId, userData);
-		console.log(response);
+
+		// Send success response with the updated user information
 		res.status(200).send({
 			message: "Successfully updated user data!",
 			result: response
@@ -86,16 +116,21 @@ const updateUser = async (req, res) => {
 	}
 };
 
+// Controller function to delete a user
 const deleteUser = async (req, res) => {
 	try {
 		let { userId } = req.params;
+
+		// Call the deleteUserData function from the userService to delete the user
 		let response = await userService.deleteUserData(userId);
 		if (response) {
+			// Send success response if the user was deleted successfully
 			res.status(200).send({
 				message: "Successfully deleted the user!",
 				result: response
 			});
 		} else {
+			// Send error response if no user was found with the given userId
 			res.status(404).send({
 				message: "No user found by the given userId!"
 			});
@@ -106,4 +141,4 @@ const deleteUser = async (req, res) => {
 	}
 };
 
-module.exports = { createUser, getUserInfo, listAllUsers, deleteUser, updateUser };
+module.exports = { createUser, getUserInfo, listAllUsers, deleteUser, updateUser }; // Export the controller functions
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,24 +1,37 @@
 const userModel = require("../model/user-model");
 
-registerUser = (payload) => {
+// Function to register a new user
+const registerUser = (payload) => {
+	// Create a new user document using the userModel and save it to the database
 	return new userModel({
 		...payload
 	}).save();
 };
 
-getAllUsers = () => {
+// Function to get all users
+const getAllUsers = () => {
+	// Find all user documents in the userModel collection
 	return userModel.find({});
 };
 
-getInfo = (_id) => {
+// Function to get user information by ID
+const getInfo = (_id) => {
+	// Find a user document with the specified _id in the userModel collection
 	return userModel.findById({ _id });
 };
 
-deleteUserData = (user) => {
+// Function to delete user data
+const deleteUserData = (user) => {
+	// Find a user document with the specified _id and delete it from the userModel collection
 	return userModel.findByIdAndDelete({ _id: user });
 };
 
-updateUser = (_id, userData) => {
+// Function to update user data
+const updateUser = (_id, userData) => {
+	// Find a user document with the specified _id and update its data with the provided userData
+	// The $set operator is used to update specific fields in the document
+	// The runValidators option ensures that the update operation follows the validation rules defined in the userModel schema
+	// The new option returns the updated document after the update operation is performed
 	return userModel.findByIdAndUpdate(
 		{ _id },
 		{
@@ -30,4 +43,4 @@ updateUser = (_id, userData) => {
 	);
 };
 
-module.exports = { registerUser, getInfo, getAllUsers, deleteUserData, updateUser };
+module.exports = { registerUser, getInfo, getAllUsers, deleteUserData, updateUser }; // Export the service functions
